ff5588ca63ae73607c05595e3ee15e94563d0d32
tindoann <tindoann@gamil.com>
2020-02-25 16:12:14 -0500

    created navbar

---
+++ src/pages/NavBar.js
@@ -0,0 +1,17 @@
+const NavBar = () => (
+  <nav>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/articles-list">Articles</Link>
+      </li>
+    </ul>
+  </nav>
+); 
+
+export default NavBar; 
