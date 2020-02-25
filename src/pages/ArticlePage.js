6891049f59a5e4f3af34f9fd2790a6188b7e5166
tindoann <tindoann@gamil.com>
2020-02-25 17:56:24 -0500

    created article-content object array

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,15 @@
 import React from 'react'; 
 
-const ArticlePage = () => (
-  <React.Fragment>
-    <h1>This is an article</h1>
-  </React.Fragment>
-); 
+const ArticlePage = ({ match }) => {
+  const name = match.params.name; 
+
+  return (
+    <React.Fragment>
+      <h1>This is the {name} article</h1>
+    </React.Fragment>
+  );
+}
 
 export default ArticlePage; 
+
+// When entering a link - the name variable will match 
