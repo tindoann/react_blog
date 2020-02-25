dabbe2d132e5f369a58cac84ba7224266e9f0b01
tindoann <tindoann@gamil.com>
2020-02-25 15:03:31 -0500

    created homepage

---
+++ src/pages/HomePages.js
@@ -0,0 +1,22 @@
+import React from 'react'; 
+
+const HomePage = () => (
+  <React.Fragment>
+  <h1>Hello, welcome to my blog!</h1>
+  <p>
+    Welcom to my blog! 
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla vero consequatur voluptatibus suscipit laboriosam nesciunt consectetur tempora quis eveniet ipsam commodi et, laudantium natus. Dicta corporis eveniet laborum voluptatum atque!
+    Ullam nesciunt autem laborum ab maxime, accusamus dolorem quibusdam rem et beatae illo tempora modi ratione in voluptate? Minus fuga, voluptas sint magni tempora et optio obcaecati reprehenderit eligendi mollitia.
+    Laudantium voluptatum culpa aperiam, nesciunt sed voluptatibus maxime sint nisi consectetur amet molestiae dolorum modi, debitis ad deleniti? Magnam sequi labore illo ut quae a enim adipisci nesciunt quia est?
+    Excepturi saepe itaque repellendus atque, alias unde incidunt exercitationem eius reprehenderit porro, dolores ullam, reiciendis neque sunt libero corrupti hic nulla quam voluptate in repudiandae! Alias ex quae excepturi? Fugiat.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla vero consequatur voluptatibus suscipit laboriosam nesciunt consectetur tempora quis eveniet ipsam commodi et, laudantium natus. Dicta corporis eveniet laborum voluptatum atque!
+    Ullam nesciunt autem laborum ab maxime, accusamus dolorem quibusdam rem et beatae illo tempora modi ratione in voluptate? Minus fuga, voluptas sint magni tempora et optio obcaecati reprehenderit eligendi mollitia.
+    Laudantium voluptatum culpa aperiam, nesciunt sed voluptatibus maxime sint nisi consectetur amet molestiae dolorum modi, debitis ad deleniti? Magnam sequi labore illo ut quae a enim adipisci nesciunt quia est?
+    Excepturi saepe itaque repellendus atque, alias unde incidunt exercitationem eius reprehenderit porro, dolores ullam, reiciendis neque sunt libero corrupti hic nulla quam voluptate in repudiandae! Alias ex quae excepturi? Fugiat.
+  </p>
+  </React.Fragment>
+); 
+
+export default HomePage; 
